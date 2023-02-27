8962f821b658a0a20180d7dfd9b57c175fb73e61
Belen Robledo <beeelrobledo@gmail.com>
2023-02-26 22:30:37 -0300

    Linked List / Floyd algorithm

--- Homework 5/linkedList.js
+++ Homework 5/linkedList.js
@@ -9,6 +9,7 @@ class LinkedList{
         this.head = null;
     }
 
+    //add to the end
     add(value){
         let node = {data: value, next: null};
 
@@ -16,7 +17,7 @@ class LinkedList{
             this.head = node;
         } else {
             let aux = this.head;
-            
+
             while(aux.next !== null){
                 aux = aux.next;
             }
@@ -24,6 +25,7 @@ class LinkedList{
         }
     }
 
+    //remove first match of given value
     remove(value){
         if(this.head){
             let aux = this.head;
@@ -31,7 +33,7 @@ class LinkedList{
             while(aux.next !== null && aux.next.data !== value){
                 aux = aux.next;
             }
-            if(aux.next.data === value){
+            if(aux.next !== null){
                 aux.next = aux.next.next;
             }
         }
@@ -44,16 +46,64 @@ class LinkedList{
             while(aux.next !== null && aux.next.data !== value){
                 aux = aux.next;
             }
-            if(aux.next.data === value){
+            if(aux.next !== null){
                 return aux.next;
             }
-
-        } else {
-            return null;
         }
+
+        return null;
     }
 
+    //Floyd algorithm with 2 pointers: slow and fast. If fast === null -> false / if fast === slow -> true
     hasLoop(){
+        if(this.head){
+            let slow = this.head;
+            let fast = this.head;
 
+            while(fast && fast.next){
+                slow = slow.next;
+                fast = fast.next.next;
+                
+                if(fast === slow){
+                    return true;
+                }
+            }
+        }
+
+        return false;
     }
 }
+
+//test
+const list = new LinkedList();
+
+list.add(1);
+list.add(5);
+list.add(6);
+list.add(7);
+
+console.log(list.hasLoop());
+
+let aux = list.head;
+while(aux != null){
+    console.log(aux.data);
+    aux = aux.next;
+}
+
+console.log(list.find(3));
+console.log(list.find(5));
+
+list.remove(7);
+
+aux = list.head;
+while(aux != null){
+    console.log(aux.data);
+    aux = aux.next;
+}
+
+list.add(7);
+
+const node7 = list.find(7);
+node7.next = list.head.next;
+
+console.log(list.hasLoop());
